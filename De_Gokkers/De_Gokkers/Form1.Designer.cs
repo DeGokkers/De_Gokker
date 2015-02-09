c7098b9b67b98ba4a1f876843512360cc74bc00d
okkeh96 <okke_trommelen@live.nl>
2015-02-09 15:55:59 +0100

    Visual Studio Solution
    De Solution

---
+++ De_Gokkers/De_Gokkers/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace De_Gokkers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Renbaan = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Renbaan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Renbaan
+            // 
+            this.Renbaan.Image = ((System.Drawing.Image)(resources.GetObject("Renbaan.Image")));
+            this.Renbaan.Location = new System.Drawing.Point(12, 12);
+            this.Renbaan.Name = "Renbaan";
+            this.Renbaan.Size = new System.Drawing.Size(594, 184);
+            this.Renbaan.TabIndex = 0;
+            this.Renbaan.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(81, 22);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 388);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Renbaan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Renbaan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Renbaan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ De_Gokkers/De_Gokkers/Greyhound.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_Gokkers
+{
+    class Greyhound
+    {
+        public int RaceTrackLength;
+        public MyPixturebox = null;
+        public Random Randomizer;
+
+        public bool Run()
+        { 
+        
+        }
+
+        public void TakeStartingPosition()
+        { 
+        
+        }
+    }
+}
