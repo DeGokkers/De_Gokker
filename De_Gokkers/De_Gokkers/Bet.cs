01b69983e3fc23aec38bd5cd7f32b5e183344140
Floris van Londen <FvLonden@gmail.com>
2015-02-10 12:37:33 +0100

    Comments

--- De_Gokkers/De_Gokkers/Bet.cs
+++ De_Gokkers/De_Gokkers/Bet.cs
@@ -13,7 +13,13 @@ namespace De_Gokkers
         public Guy Bettor;
 
         public string GetDescription()
-        { 
+        {
+            //Retourneer een string die aangeeft wie de weddenschap heeft gedaan,
+            //voor welk bedrag er is gewed en op welke hond er is gewed.
+            //Bijv. “Sietse wedt 8 euro op hond 4”.
+            //Als het bedrag 0 is, is er geen weddenschap geplaatst.
+            //De string die dan geretourneerd wordt is bijv.
+            //“Sietse heeft geen weddenschap geplaatst.”)
             string iets = "iets";
             return iets;
         }
@@ -23,5 +29,6 @@ namespace De_Gokkers
             int iets = 1;
             return iets;
         }
+
     }
 }
