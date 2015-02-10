271c4d5ff5bcd428c8e49aa14c8fe09fe3bf46b0
Tom <d167647@edu.rocwb.nl>
2015-02-10 12:01:41 +0100

    N00b

--- De_Gokkers/De_Gokkers/Form1.Designer.cs
+++ De_Gokkers/De_Gokkers/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.SuspendLayout();
             // 
             // Main
@@ -35,8 +36,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hazenrace";
             this.ResumeLayout(false);
 
         }
