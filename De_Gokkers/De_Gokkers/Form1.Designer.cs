75daaaa652be9dbfcac8f30f812ee067608c9d2f
Tom <d167647@edu.rocwb.nl>
2015-02-10 12:44:17 +0100

    Design

--- De_Gokkers/De_Gokkers/Form1.Designer.cs
+++ De_Gokkers/De_Gokkers/Form1.Designer.cs
@@ -29,22 +29,37 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.Racecourse = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Racecourse)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Racecourse
+            // 
+            this.Racecourse.Image = global::De_Gokkers.Properties.Resources.renbaan1;
+            this.Racecourse.Location = new System.Drawing.Point(12, 12);
+            this.Racecourse.Name = "Racecourse";
+            this.Racecourse.Size = new System.Drawing.Size(1003, 496);
+            this.Racecourse.TabIndex = 0;
+            this.Racecourse.TabStop = false;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1007, 482);
+            this.Controls.Add(this.Racecourse);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hazenrace";
+            ((System.ComponentModel.ISupportInitialize)(this.Racecourse)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Racecourse;
     }
 }
 
