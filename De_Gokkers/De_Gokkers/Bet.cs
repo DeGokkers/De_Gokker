59d62702c3b4573789540579f0b67382400d75f6
Floris van Londen <FvLonden@gmail.com>
2015-02-10 10:37:14 +0100

    Revert "New"
    This reverts commit 9e91c31eedbc80d27ed87540371d737924e5f655.

---
+++ De_Gokkers/De_Gokkers/Bet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_Gokkers
+{
+    class Bet
+    {
+        public int Amount;
+        public int Dog;
+        public Guy Bettor;
+
+        public string GetDescription()
+        { 
+            string iets = "iets";
+            return iets;
+        }
+
+        public int PayOut(int Winner)
+        { 
+            int iets = 1;
+            return iets;
+        }
+    }
+}
---
+++ De_Gokkers/De_Gokkers/Greyhound.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace De_Gokkers
+{
+    class Greyhound
+    {
+        public int RaceTrackLength;
+        public PictureBox MyPictureBox = null;
+        public Random Randomizer;
+
+        public bool Run()
+        {
+            return true;
+        }
+
+        public void TakeStartingPosition()
+        { 
+        
+        }
+    }
+}
