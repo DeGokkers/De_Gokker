b054f494acb54f697cea77228ac17c20b3b31efa
Tom <d167647@edu.rocwb.nl>
2015-02-10 11:57:57 +0100

    Program ICON Added

--- De_Gokkers/De_Gokkers/Form1.cs
+++ De_Gokkers/De_Gokkers/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace De_Gokkers
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
