a2885b6191cc87fa228741e96145cc071a94edd9
okkeh96 <okke_trommelen@live.nl>
2015-02-10 11:57:00 +0100

    Run gemaakt, hopelijk
    Run gemaakt

--- De_Gokkers/De_Gokkers/Greyhound.cs
+++ De_Gokkers/De_Gokkers/Greyhound.cs
@@ -10,11 +10,14 @@ namespace De_Gokkers
     class Greyhound
     {
         public int RaceTrackLength;
-        public PictureBox MyPictureBox = null;
+        static Form1 test = new Form1();
+        public PictureBox MyPictureBox = test.pictureBox1;
         public Random Randomizer;
 
         public void Run()
         {
+            int distance = Randomizer.Next(1, 4);
+            MyPictureBox.Location = new Point(MyPictureBox.Location.X + distance, MyPictureBox.Location.Y);
         }
 
         public void TakeStartingPosition()
