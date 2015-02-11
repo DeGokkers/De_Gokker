1d5ae8e83687f24a8edfd289462aa06a96295988
okkeh96 <okke_trommelen@live.nl>
2015-02-11 16:44:31 +0100

    Upgedate Solution
    Solution update

---
+++ De_Gokkers/De_Gokkers - update/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace De_Gokkers
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            _Main = this;
+        }
+        public static Main _Main;
+
+        public void update(string message)
+        {
+            txt_Announce.Text = message;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Guy sample = new Guy();
+        }
+
+        public decimal amount()
+        {
+            return num_AmountMoney.Value;
+        }
+    }
+}
